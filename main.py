b0a6a2b1ff3d9c0a60df2beab53f86025f4fecdd
Benjamin Miller <benhmiller22@gmail.com>
2024-04-25 13:30:36 -0500

    Initial coding solution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#   Author        : *** INSERT YOUR NAME ***
-#   Last Modified : *** DATE ***
+#   Author        : Benjamin Miller
+#   Last Modified : 04 / 18 / 2024
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,12 +11,65 @@ from sklearn import tree
 from sklearn import metrics
 from sklearn import neural_network
 from sklearn import model_selection
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 
 
 def exp1(data, labels):
     """STUDENT CODE BELOW"""
     # define model architecure
-    model = tree.DecisionTreeClassifier(max_depth=1)
+    # model = tree.DecisionTreeClassifier(max_depth=1)
+    # print(data.shape, labels.shape)
+
+    # Prepare local variables
+    X = data
+    y = labels
+    tree_sizes = range(10,16)
+
+    # Split Data Into Train, Test, and Validation Sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Determine optimal tree depth using cross validation
+    cv_scores = []
+    for depth in tree_sizes:
+        # Create decision tree classifier with current size
+        clf = tree.DecisionTreeClassifier(max_depth=depth, random_state=42)
+
+        # Perform 5-fold cross-validation on the training set
+        scores = cross_val_score(clf, X_train, y_train, cv=5)
+
+        # Compute average accuracy across flds
+        avg_score = np.mean(scores)
+
+        # Append score to list
+        cv_scores.append(avg_score)
+
+    # Determine Optimal Tree Size and Score
+    optimal_size_index = np.argmax(cv_scores)
+    optimal_size = tree_sizes[optimal_size_index]
+    optimal_score = cv_scores[optimal_size_index]
+
+    print("Optimal Tree Size:", optimal_size)
+    print("Average Cross-Validation Accuracy:", optimal_score)
+
+    # Plot Accuracy vs. Tree Depth
+    plt.plot(tree_sizes, cv_scores, marker='o')
+    plt.title('Accuracy vs. Tree Depth')
+    plt.xlabel('Tree Depth')
+    plt.ylabel('Cross-Validation Accuracy')
+
+    # Save the plot as an image file
+    plt.savefig('figures/accuracy_vs_tree_depth.png')
+
+    # Show the plot (optional)
+    plt.show()
+
+    # Train final model using the optimal tree size on the entire training set
+    model = tree.DecisionTreeClassifier(max_depth=optimal_size)
+    model.fit(X_train, y_train)
+
+    # Evaluate final model on the test set
+    test_accuracy = model.score(X_test, y_test)
+    print("Test Set Accuracy:", test_accuracy)
     """STUDENT CODE ABOVE"""
     return model
 
@@ -24,7 +77,46 @@ def exp1(data, labels):
 def exp2(data, labels):
     """STUDENT CODE BELOW"""
     # define model architecture
-    model = tree.DecisionTreeClassifier(max_depth=1)
+    #model = tree.DecisionTreeClassifier(max_depth=1)
+
+    # Prepare local variables
+    X = data
+    y = labels
+    tree_sizes = range(5,15)
+
+    # Split Data Into Train, Test, and Validation Sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Determine optimal tree depth using cross validation
+    cv_scores = []
+    for depth in tree_sizes:
+        # Create decision tree classifier with current size
+        clf = tree.DecisionTreeClassifier(max_depth=depth)
+
+        # Perform 5-fold cross-validation on the training set
+        scores = cross_val_score(clf, X_train, y_train, cv=5)
+
+        # Compute average accuracy across flds
+        avg_score = np.mean(scores)
+
+        # Append score to list
+        cv_scores.append(avg_score)
+
+    # Determine Optimal Tree Size and Score
+    optimal_size_index = np.argmax(cv_scores)
+    optimal_size = tree_sizes[optimal_size_index]
+    optimal_score = cv_scores[optimal_size_index]
+
+    print("Optimal Tree Size:", optimal_size)
+    print("Average Cross-Validation Accuracy:", optimal_score)
+
+    # Train final model using the optimal tree size on the entire training set
+    model = tree.DecisionTreeClassifier(max_depth=optimal_size)
+    model.fit(X_train, y_train)
+
+    # Evaluate final model on the test set
+    test_accuracy = model.score(X_test, y_test)
+    print("Test Set Accuracy:", test_accuracy)
     """STUDENT CODE ABOVE"""
     return model
 
@@ -33,14 +125,76 @@ def exp3(data, labels):
     """STUDENT CODE BELOW"""
     # define model architecture
     model = neural_network.MLPClassifier(hidden_layer_sizes=(50,))
+
+    # Prepare local variables
+    X = data
+    y = labels
+
+    # Train-Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Train Model (with default values)
+    model.fit(X_train, y_train)
+
+    # Evaluate final model on the test set
+    test_accuracy = model.score(X_test, y_test)
+    print("Test Set Accuracy:", test_accuracy)
+
     """STUDENT CODE ABOVE"""
     return model
 
 
 def exp4(data, labels):
     """STUDENT CODE BELOW"""
+    
     # define model architecture
-    model = neural_network.MLPClassifier(hidden_layer_sizes=(50,))
+    model = neural_network.MLPClassifier(hidden_layer_sizes=(100,))
+    #'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
+
+    # Prepare local variables
+    X = data
+    y = labels
+
+    # Train-Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Train Model (with default values)
+    model.fit(X_train, y_train)
+
+    # Evaluate final model on the test set
+    test_accuracy = model.score(X_test, y_test)
+    print("Test Set Accuracy:", test_accuracy)
+    '''
+    # Prepare local variables
+    X = data
+    y = labels
+
+    # Train-Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Define MLPClassifier
+    mlp = neural_network.MLPClassifier()
+
+    # Define parameter grid for hyperparameter tuning
+    param_grid = {
+        #'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
+        #'activation': ['relu', 'tanh', 'logistic'],
+        #'alpha': [0.0001, 0.001, 0.01, 0.1],
+        #'learning_rate_init': [0.001, 0.01, 0.1]
+    }
+
+    # Perform GridSearchCV
+    grid_search = GridSearchCV(mlp, param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+
+    # Get best model and its parameters
+    model = grid_search.best_estimator_
+    best_params = grid_search.best_params_
+
+    # Evaluate final model on the test set
+    test_accuracy = model.score(X_test, y_test)
+    print("Test Set Accuracy:", test_accuracy)
+    '''
     """STUDENT CODE ABOVE"""
     return model
 
--- write-up/experiment_questions.md
+++ write-up/experiment_questions.md
@@ -4,6 +4,8 @@ Complete with command to run on test set: `./642test-$(uname -p) <MODEL_FILE_NAM
 
 ### Q1: What hyperparameters did you vary, and what values did you try? What were some trends you observed?
 
+Within the first experiment concerning the binary classification performed by a decision tree, I opted to set the random_state to 42 for the sake of consistency across multiple runs, then performed cross validation testing while varying the max_depth hyperparameter. In order to ascertain a better unerstanding of how the depth affected the model, I plotted cross-validation accuracy vs. the varying values of maximum tree depth and observed the relationship. Ultimately, after noticing a steep increase in accuracy around depths 5-7 and a plateau of accuracy around depths 13-14, I opted to test a range of depths from 10-15. Though accuracy marginally increased in the range 20-30 (often peaking around 24-26), I felt that limiting the depth to this shorter range would better avoid overfitting and lead to a more accurate model when testing on unseen data.
+
 ### Q2: Provide an image of the trained decision tree. Discuss what you observe.
 
 ![placeholder image](placeholder.png)
